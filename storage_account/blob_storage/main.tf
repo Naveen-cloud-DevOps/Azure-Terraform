9c66152b52a83366b471a311a5349d6b907f983e
Naveen <150805268+Naveen-cloud-DevOps@users.noreply.github.com>
2025-06-30 17:28:57 +0530

    Update main.tf

--- storage_account/blob_storage/main.tf
+++ storage_account/blob_storage/main.tf
@@ -17,13 +17,13 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "public-container"
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_id = azurerm_storage_account.storage.id
   container_access_type = "blob"
 }
 
 resource "azurerm_storage_blob" "example_blob" {
   name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.storage.name
+  storage_account_id = azurerm_storage_account.storage.id
   storage_container_name = azurerm_storage_container.container.name
   type                   = "Block"
   source                 = "${path.module}/index.html" # make sure this file exists
